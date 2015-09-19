6380045107dfeae8b90a376e107739ca682261df
yyakovlev <yyakovlev@luxoft.com>
2015-09-19 23:28:09 +0300

    [Prototype] Step 1: Create "Echo" prototype which echoes message received from UI back to client, without using live broker and modules

---
+++ env-monitor/env-monitor-ui/src/main/java/org/envtools/monitor/ui/app/Application.java
@@ -0,0 +1,17 @@
+package org.envtools.monitor.envtools.monitor.ui.app;
+
+/**
+ * Created: 9/19/15 11:51 PM
+ *
+ * @author Yury Yakovlev
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ env-monitor/env-monitor-ui/src/main/java/org/envtools/monitor/ui/controller/WebSocketAppController.java
@@ -0,0 +1,9 @@
+package org.envtools.monitor.envtools.monitor.ui.controller;
+
+/**
+ * Created: 9/19/15 11:50 PM
+ *
+ * @author Yury Yakovlev
+ */
+public class WebSocketAppController {
+}
