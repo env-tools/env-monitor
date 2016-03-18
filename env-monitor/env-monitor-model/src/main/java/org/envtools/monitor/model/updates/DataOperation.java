fdf89fed58c269e2532ab8b3e6ec40826863eb76
jesa29@rambler.ru <mf1993@rambler.ru>
2016-03-18 21:13:48 +0300

    DataOperation object corrections #EM-24

--- env-monitor/env-monitor-model/src/main/java/org/envtools/monitor/model/updates/DataOperation.java
+++ env-monitor/env-monitor-model/src/main/java/org/envtools/monitor/model/updates/DataOperation.java
@@ -1,12 +1,56 @@
 package org.envtools.monitor.model.updates;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 import java.util.Map;
 
 /**
  * Created by jesa on 18.03.2016.
  */
 public class DataOperation {
-    enum DataOperationType {CREATE,UPDATE,DELETE}; //available types of operations
-    String entity;
-    Map<String,String> fields;
+    private DataOperationType type; //available types of operations
+    private String entity;
+    private Map<String, String> fields;
+
+    public DataOperation() {
+    }
+
+    public DataOperation(DataOperationType type, String entity, Map<String, String> fields) {
+        this.type = type;
+        this.entity = entity;
+        this.fields = fields;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public Map<String, String> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<String, String> fields) {
+        this.fields = fields;
+    }
+
+    public DataOperationType getType() {
+        return type;
+    }
+
+    public void setType(DataOperationType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("type", type)
+                .append("entity", entity)
+                .append("fields", fields)
+                .toString();
+    }
 }
---
+++ env-monitor/env-monitor-model/src/main/java/org/envtools/monitor/model/updates/DataOperationType.java
@@ -0,0 +1,8 @@
+package org.envtools.monitor.model.updates;
+
+/**
+ * Created by jesa on 18.03.2016.
+ */
+public enum DataOperationType {
+    CREATE, UPDATE, DELETE
+}
