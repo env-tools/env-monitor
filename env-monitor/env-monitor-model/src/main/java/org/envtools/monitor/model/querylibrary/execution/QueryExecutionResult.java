2136ecb7b6e2e56a32c41cfc2583b3ab371c8553
yyakovlev <yyakovlev@luxoft.com>
2016-03-19 10:36:10 +0300

    Refactor/cleanup prototype code and provide comments - query execution service #EM-7

--- env-monitor/env-monitor-model/src/main/java/org/envtools/monitor/model/querylibrary/execution/QueryExecutionResult.java
+++ env-monitor/env-monitor-model/src/main/java/org/envtools/monitor/model/querylibrary/execution/QueryExecutionResult.java
@@ -17,15 +17,45 @@ public class QueryExecutionResult {
         COMPLETED, HAS_MORE_DATA, ERROR, TIMED_OUT
     }
 
+    /**
+     * What was the outcome of query execution
+     */
+    private final ExecutionStatusE status;
+
+
+    /**
+     * How much time it took to execute lastly
+     * Say: executed 20 sec initially (1st stage, returned 50 rows) ,
+     *  3 sec when user requested more rows (2nd stage, returned 24 rows).
+     *  So there will be first result with elapsedTimeMs = 20000,
+     *  and for the second request there will be second result with elapsedTimeMs = 3000
+     */
     private final long elapsedTimeMs;
+
+    /**
+     * How many rows returned for this stage.
+     * For the example above 2 results will be produced,
+     * with returnedRowCount=50 for 1st stage and returnedRowCount=24 for 2nd stage
+     */
     private final long returnedRowCount;
 
+    /**
+     * Result rows as provided by the ResultSet object
+     */
     private final List<Map<String, Object>> resultRows;
 
+    /**
+     * Detailed error message or Optional.absent() if there was no error
+     */
     private final Optional<String> errorMessage;
+
+    /**
+     * Exception occurred or Optional.absent() if there was no exception
+     */
     private final Optional<Throwable> error;
 
-    public QueryExecutionResult(long elapsedTimeMs, long returnedRowCount, List<Map<String, Object>> resultRows, Optional<String> errorMessage, Optional<Throwable> error) {
+    public QueryExecutionResult(ExecutionStatusE status, long elapsedTimeMs, long returnedRowCount, List<Map<String, Object>> resultRows, Optional<String> errorMessage, Optional<Throwable> error) {
+        this.status = status;
         this.elapsedTimeMs = elapsedTimeMs;
         this.returnedRowCount = returnedRowCount;
         this.resultRows = resultRows;
