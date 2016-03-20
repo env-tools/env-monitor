ee54c6bb66724267150f4a535cc8aedcad334ca1
anastasiya14 <nastya19922008@gmail.com>
2016-03-21 00:54:37 +0300

    Наброски реализации Query Execution в JdbcQueryExecutionTask  #EM-20

--- env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/services/impl/execution/JdbcQueryExecutionTask.java
+++ env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/services/impl/execution/JdbcQueryExecutionTask.java
@@ -1,14 +1,33 @@
 package org.envtools.monitor.module.querylibrary.services.impl.execution;
 
+
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.log4j.Logger;
+import org.envtools.monitor.model.querylibrary.DataProviderType;
+import org.envtools.monitor.model.querylibrary.db.Category;
+import org.envtools.monitor.model.querylibrary.execution.QueryExecutionNextResultRequest;
 import org.envtools.monitor.model.querylibrary.execution.QueryExecutionRequest;
 import org.envtools.monitor.model.querylibrary.execution.QueryExecutionResult;
 import org.envtools.monitor.module.querylibrary.services.DataSourceService;
-
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.RowMapperResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.envtools.monitor.model.querylibrary.execution.QueryExecutionResult.ExecutionStatusE;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.envtools.monitor.model.querylibrary.execution.QueryExecutionResult.ExecutionStatusE.*;
 
 /**
  * Created: 13.03.16 1:34
@@ -16,8 +35,10 @@ import java.sql.SQLException;
  * @author Yury Yakovlev
  */
 public class JdbcQueryExecutionTask extends AbstractQueryExecutionTask{
-
+    private static final Logger LOGGER = Logger.getLogger(JdbcQueryExecutionTask.class);
     private BasicDataSource jdbcDataSource;
+    private static final String selectSql = "SELECT * FROM Category";
+
 
     public JdbcQueryExecutionTask(
             QueryExecutionRequest queryExecutionRequest,
@@ -32,9 +53,12 @@ public class JdbcQueryExecutionTask extends AbstractQueryExecutionTask{
     @Override
     protected QueryExecutionResult doCall() {
 
-        QueryExecutionResult queryExecutionResult = null;
+        DataProviderType  queryType;
+        QueryExecutionRequest queryExecutionRequest=null;
+        QueryExecutionResult  queryExecutionResult=null;
+        final ExecutionStatusE[] status = new ExecutionStatusE[1];
 
-        try (Connection connection = jdbcDataSource.getConnection()){
+       /* try (Connection connection = jdbcDataSource.getConnection()){
             //TODO implement query execution
             //Line below is for testing only
             ResultSet rs = connection
@@ -47,8 +71,43 @@ public class JdbcQueryExecutionTask extends AbstractQueryExecutionTask{
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        */
+        NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(jdbcDataSource);
+        /*Получим n строк с запросом queryExecutionRequest.getQuery()
+        * с параметрами queryExecutionRequest.getQueryParameters(),
+        * из базы c параметрами jdbcDataSource*/
+        jdbcTemplate.query(queryExecutionRequest.getQuery(),queryExecutionRequest.getQueryParameters(),
+                new ResultSetExtractor<List<Map<String, Object>>>(){
+                    public List<Map<String, Object>> extractData(ResultSet rs) throws SQLException, DataAccessException {
+                        int rowNum = 0; //строки
+                        try {
+                            ResultSetMetaData md = rs.getMetaData();
+                            int columns = md.getColumnCount();
+                            List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
+                            while (rs.next()&& queryExecutionRequest.getRowCount()>rowNum){
+                                Map<String, Object> row = new HashMap<String, Object>(columns);
 
+                                    row.put(md.getColumnName(rowNum), rs.getObject(rowNum));
+                                rowNum++;
+                                rows.add(row);
+                            }
+                            LOGGER.info("Found queries: " + ExecutionStatusE.COMPLETED);
+                            status[0] =ExecutionStatusE.COMPLETED;
+                            return rows;
+                        }catch (SQLException wx){
+                            LOGGER.info("Found queries: " + ExecutionStatusE.ERROR);
+                            status[0] =ExecutionStatusE.ERROR;
+                        }
+
+                        return null;
+                    }
+  //надо получить время, статус, количество полученных строк,List<Map<String, Object>>resultRows,
+                    // errorMessage, Optional<Throwable> error
+        });
         //Currently mock
         return queryExecutionResult;
     }
+
+
+
 }
