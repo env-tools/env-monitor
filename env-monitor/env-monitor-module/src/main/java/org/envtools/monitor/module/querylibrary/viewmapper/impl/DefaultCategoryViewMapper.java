e50a1873ffbb8c261037fe37e94b2509fc18379e
jesa29@rambler.ru <mf1993@rambler.ru>
2016-04-02 15:07:10 +0300

    Класс DefaultCategoryViewMapper частично #EM-32

--- env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/viewmapper/impl/DefaultCategoryViewMapper.java
+++ env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/viewmapper/impl/DefaultCategoryViewMapper.java
@@ -1,21 +1,31 @@
 package org.envtools.monitor.module.querylibrary.viewmapper.impl;
 
+import com.google.common.collect.Maps;
 import org.envtools.monitor.model.querylibrary.db.Category;
 import org.envtools.monitor.model.querylibrary.tree.view.CategoryView;
 import org.envtools.monitor.module.querylibrary.viewmapper.CategoryViewMapper;
 
 import java.util.Map;
 
+
 /**
  * Created: 01.04.16 22:05
  *
  * @author Yury Yakovlev
  */
-public class DefaultCategoryViewMapper implements CategoryViewMapper{
+public class DefaultCategoryViewMapper implements CategoryViewMapper {
 
     @Override
     public Map<String, CategoryView> mapCategoriesByOwner(Map<String, Category> categoriesByOwner) {
         //TODO implement
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        Map<String, CategoryView> view = Maps.newLinkedHashMap();
+        for (Map.Entry<String, Category> entry : categoriesByOwner.entrySet()) {
+            view.put(entry.getKey(), mapList(entry.getValue()));
+        }
+        return view;
+    }
+
+    private CategoryView mapList(Category categoryByOwner) {
+        return null;
     }
 }
