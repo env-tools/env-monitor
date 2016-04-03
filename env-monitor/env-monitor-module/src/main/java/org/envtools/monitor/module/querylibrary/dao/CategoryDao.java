1afe5c21491541b0e49ebb5a12ee7ef0e5d64b4a
yyakovlev <l1234567890@yandex.ru>
2016-04-03 13:22:37 +0300

    fix compilation#EM-7

--- env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/dao/CategoryDao.java
+++ env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/querylibrary/dao/CategoryDao.java
@@ -7,11 +7,9 @@ import java.util.List;
 /**
  * Created by anastasiya on 06.03.16.
  */
-public interface CategoryDao   extends Dao<Category, Long> {
+public interface CategoryDao extends Dao<Category, Long> {
 
     List<Category> getCategoryByTitle(String title);
 
     List<Category> getRootCategories();
-   List<Category> getRootCategoriesByOwner(String owner);
-
 }
