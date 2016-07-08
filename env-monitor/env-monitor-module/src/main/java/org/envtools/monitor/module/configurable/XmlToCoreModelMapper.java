4ce03066f74cecf88eafe9a3ebe50992576c3395
Michal <michal@allocationerror.com>
2016-07-08 12:07:26 +0200

    XmlToCoreModelMapper #EM-60

--- env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/configurable/XmlToCoreModelMapper.java
+++ env-monitor/env-monitor-module/src/main/java/org/envtools/monitor/module/configurable/XmlToCoreModelMapper.java
@@ -1,6 +1,8 @@
 package org.envtools.monitor.module.configurable;
 
 import org.envtools.monitor.model.applications.*;
+import org.envtools.monitor.model.applications.Environment;
+import org.envtools.monitor.model.applications.Platform;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,57 +13,76 @@ import java.util.List;
 public class XmlToCoreModelMapper {
     public static ApplicationsData convertToApplicationsData(VersionedApplicationProperties applicationProperties) {
         ApplicationsData applicationsData = new ApplicationsData();
+        applicationsData.setPlatforms(mapPlatforms(applicationProperties));
+        return applicationsData;
+    }
 
+    private static List<Platform>  mapPlatforms(VersionedApplicationProperties applicationProperties) {
         List<Platform> platforms = new ArrayList<>();
-
         for (org.envtools.monitor.module.configurable.applicationsMetadata.Platform platform : applicationProperties.getPlatforms()) {
-
-            ArrayList<Environment> environments = new ArrayList<>();
-
-            for (org.envtools.monitor.module.configurable.applicationsMetadata.Environment environment : platform.getEnvironments()) {
-                List<Application> applications = new ArrayList<>();
-
-                for (VersionedApplication versionedApplication : environment.getApplications()) {
-                    Application application = new Application();
-                    application.setName(versionedApplication.getName());
-                    application.setId(versionedApplication.getId());
-                    application.setStatus(ApplicationStatus.RUNNING);
-
-                    ArrayList<Application> hostees = new ArrayList<>();
-
-                    if (versionedApplication.getHostees() != null) {
-                        for (VersionedApplication hosteeApp : versionedApplication.getHostees()) {
-                            Application hostee = new Application();
-                            hostee.setName(hosteeApp.getName());
-                            hostee.setId(hosteeApp.getId());
-                            hostee.setStatus(ApplicationStatus.RUNNING);
-
-                            hostees.add(hostee);
-                        }
-                    }
-
-                    application.setHostees(hostees);
-                    applications.add(application);
-                }
-
-                Environment environmentTmp = new Environment();
-                environmentTmp.setId(environment.getId());
-                environmentTmp.setName(environment.getName());
-                environmentTmp.setApplications(applications);
-                environments.add(environmentTmp);
-            }
-
-
-            Platform platformTmp = new Platform();
-            platformTmp.setId(platform.getId());
-            platformTmp.setName(platform.getName());
-            platformTmp.setEnvironments(environments);
-            platforms.add(platformTmp);
+            platforms.add(convertPlatform(platform));
         }
 
-        applicationsData.setPlatforms(platforms);
+        return platforms;
+    }
 
+    private static ArrayList<Environment> mapEnvironments(org.envtools.monitor.module.configurable.applicationsMetadata.Platform platform) {
+        ArrayList<Environment> environments = new ArrayList<Environment>();
+        for (org.envtools.monitor.module.configurable.applicationsMetadata.Environment environment : platform.getEnvironments()) {
+            environments.add(convertEnvironment(environment));
+        }
+        return environments;
+    }
 
-        return applicationsData;
+    private static List<Application> mapApplications(org.envtools.monitor.module.configurable.applicationsMetadata.Environment environment) {
+        List<Application> applications = new ArrayList<>();
+        for (VersionedApplication versionedApplication : environment.getApplications()) {
+            applications.add(convertApplication(versionedApplication));
+        }
+        return applications;
+    }
+
+    private static ArrayList<Application> mapHostees(VersionedApplication versionedApplication) {
+        ArrayList<Application> hostees = new ArrayList<>();
+
+        if (versionedApplication.getHostees() != null) {
+            for (VersionedApplication hosteeApp : versionedApplication.getHostees()) {
+                hostees.add(convertHostee(hosteeApp));
+            }
+        }
+        return hostees;
+    }
+
+    private static Platform convertPlatform(org.envtools.monitor.module.configurable.applicationsMetadata.Platform platform) {
+        Platform platformTmp = new Platform();
+        platformTmp.setId(platform.getId());
+        platformTmp.setName(platform.getName());
+        platformTmp.setEnvironments(mapEnvironments(platform));
+        return platformTmp;
+    }
+
+    private static Environment convertEnvironment(org.envtools.monitor.module.configurable.applicationsMetadata.Environment environment) {
+        Environment environmentTmp = new Environment();
+        environmentTmp.setId(environment.getId());
+        environmentTmp.setName(environment.getName());
+        environmentTmp.setApplications(mapApplications(environment));
+        return environmentTmp;
+    }
+
+    private static Application convertApplication(VersionedApplication versionedApplication) {
+        Application application = new Application();
+        application.setName(versionedApplication.getName());
+        application.setId(versionedApplication.getId());
+        application.setStatus(ApplicationStatus.RUNNING);
+        application.setHostees(mapHostees(versionedApplication));
+        return application;
+    }
+
+    private static Application convertHostee(VersionedApplication hosteeApp) {
+        Application hostee = new Application();
+        hostee.setName(hosteeApp.getName());
+        hostee.setId(hosteeApp.getId());
+        hostee.setStatus(ApplicationStatus.RUNNING);
+        return hostee;
     }
 }
