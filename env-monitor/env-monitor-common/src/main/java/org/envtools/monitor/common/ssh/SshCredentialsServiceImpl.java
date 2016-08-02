fe070fb47059371e3b44f0a7c199d76a3f6baa57
Michal <michal@allocationerror.com>
2016-08-02 13:46:41 +0200

    Cleanup of SshCredentialsServiceImpl #EM-72

--- env-monitor/env-monitor-common/src/main/java/org/envtools/monitor/common/ssh/SshCredentialsServiceImpl.java
+++ env-monitor/env-monitor-common/src/main/java/org/envtools/monitor/common/ssh/SshCredentialsServiceImpl.java
@@ -15,24 +15,6 @@ import java.util.Properties;
  */
 public class SshCredentialsServiceImpl implements SshCredentialsService {
     private final String directoryPath;
-    private Properties prop;
-
-//    private static final String PROP_FILE = "src/main/resources/application.properties";
-//    private static final String MASTER_KEY = "jasypt.master.key";
-//    private static final String ALGORITHM = "jasypt.algorithm";
-
-//    {
-//        prop = new Properties();
-//
-//        try (InputStream input = new FileInputStream(PROP_FILE)) {
-//
-//            prop.load(input);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    private final EncryptionServiceImpl encryptionService = new EncryptionServiceImpl(prop.getProperty(MASTER_KEY), prop.getProperty(ALGORITHM));
 
     public SshCredentialsServiceImpl(String credentialsDirectory) {
         this.directoryPath = credentialsDirectory;
@@ -42,7 +24,6 @@ public class SshCredentialsServiceImpl implements SshCredentialsService {
     public Credentials getCredentials(String host) {
         try {
             Credentials credentials = JaxbHelper.unmarshallFromFile(getCredentialFile(host), Credentials.class, null);
-//            credentials.setPassword(encryptionService.decrypt(credentials.getEncryptedPassword()));
             credentials.setPassword(credentials.getEncryptedPassword());
             return credentials;
 
