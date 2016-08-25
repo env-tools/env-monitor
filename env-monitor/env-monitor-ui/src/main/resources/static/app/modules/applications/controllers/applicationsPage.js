b6ee5aca98f28ef63c7b09d5c91bd91fba87a847
Michal <mmsqzi@gmail.com>
2016-08-25 16:33:16 +0200

    STOPPED/RUNNING rows with different colors #EM-72

--- env-monitor/env-monitor-ui/src/main/resources/static/app/modules/applications/controllers/applicationsPage.js
+++ env-monitor/env-monitor-ui/src/main/resources/static/app/modules/applications/controllers/applicationsPage.js
@@ -39,13 +39,17 @@
                     {name: "Version", field: "version", width: "150", resizable: true},
                     {name: "Component Name", field: "componentName", width: "150", resizable: true},
                     {name: "Memory (Mb)", field: "processMemory", width: "150", resizable: true},
-                    {name: "Status", field: "status", width: "150", resizable: true}
+                    {
+                        name: "Status", field: "status", width: "150", resizable: true,
+                        cellClass: function (grid, row, col, rowRenderIndex, colRenderIndex) {
+                            return grid.getCellValue(row, col) === 'RUNNING' ? 'green' : 'red';
+                        }
+                    }
                 ],
 
                 showTreeExpandNoChildren: false,
 
-                onRegisterApi: function(gridApi)
-                {
+                onRegisterApi: function (gridApi) {
                     $scope.gridApi = gridApi;
                 }
             };
@@ -60,15 +64,15 @@
                     var pattern = '/call/modules/M_APPLICATIONS/data/platforms/{platformId}/environments';
                     ngstomp.subscribeTo(pattern.replace('{platformId}', $scope.filters.selectedPlatform.id))
                         .callback(
-                            function (msg) {
-                                $scope.environments = msg.body.payload.jsonContent;
-                                $scope.filters.selectedEnvironment = $scope.environments[0];
-                                console.log('Call result for environments: ' + JSON.stringify(msg.body.payload.jsonContent));
+                        function (msg) {
+                            $scope.environments = msg.body.payload.jsonContent;
+                            $scope.filters.selectedEnvironment = $scope.environments[0];
+                            console.log('Call result for environments: ' + JSON.stringify(msg.body.payload.jsonContent));
 
-                                $scope.selectedEnvironmentChanged();
+                            $scope.selectedEnvironmentChanged();
 
-                            }).withBodyInJson().connect();
-            }).withBodyInJson().connect();
+                        }).withBodyInJson().connect();
+                }).withBodyInJson().connect();
         }
 
         $scope.selectedPlatformChanged = function () {
@@ -101,9 +105,9 @@
             $scope.currentApplicationsSubscribeId = ngstomp.subscribeTo(
                 pattern
                     .replace("{platformId}",
-                        $scope.filters.selectedPlatform.id)
+                    $scope.filters.selectedPlatform.id)
                     .replace("{environmentId}",
-                        $scope.filters.selectedEnvironment.id))
+                    $scope.filters.selectedEnvironment.id))
                 .callback(function (msg) {
                     $scope.applications = msg.body.payload.jsonContent;
                     $scope.gridOptions.data = convertToDataForGrid($scope.applications);
@@ -113,48 +117,48 @@
 //                                    {"name":"app2_1-1","applicationType":"applicationType2","host":"host2","port":7001,"url":"http://host1:7001/app/login","version":"1.12_E20","componentName":"component-2-1-1","processMemory":11335,"status":"RUNNING", $$treeLevel : 1}
 //                                ],
 
-                        //$scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.ALL);
-                        function convertToDataForGrid(applications) {
-                            var dataForGrid = [];
+                    //$scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.ALL);
+                    function convertToDataForGrid(applications) {
+                        var dataForGrid = [];
 
-                            //Currently we support only one level of nesting
-                            angular.forEach(applications, function(application, index) {
+                        //Currently we support only one level of nesting
+                        angular.forEach(applications, function (application, index) {
 
-                                var collector = this;
-                                addApplicationForGrid(collector, application, 0);
+                            var collector = this;
+                            addApplicationForGrid(collector, application, 0);
 
-                                //Go into depth and process hostees (nested applications)
-                                if (application.hostees && application.hostees.length > 0) {
-                                    angular.forEach(application.hostees, function(hostee, index) {
-                                        addApplicationForGrid(collector, hostee, 1);
-                                    }, collector);
-                                }
-
-                            }, dataForGrid);
-
-                            console.log('dataForGrid=' + JSON.stringify(dataForGrid));
-
-                            function addApplicationForGrid(applicationsForGrid, application, level) {
-                                var applicationForGrid = {
-                                  name : application.name,
-                                  applicationType : application.applicationType,
-                                  host : application.host,
-                                  port : application.port,
-                                  url : application.url,
-                                  version : application.version,
-                                  componentName : application.componentName,
-                                  processMemory : application.processMemory,
-                                  status : application.status,
-                                    //,
-
-                                  $$treeLevel : level
-                                };
-
-                                applicationsForGrid.push(applicationForGrid);
+                            //Go into depth and process hostees (nested applications)
+                            if (application.hostees && application.hostees.length > 0) {
+                                angular.forEach(application.hostees, function (hostee, index) {
+                                    addApplicationForGrid(collector, hostee, 1);
+                                }, collector);
                             }
 
-                            return dataForGrid;
+                        }, dataForGrid);
+
+                        console.log('dataForGrid=' + JSON.stringify(dataForGrid));
+
+                        function addApplicationForGrid(applicationsForGrid, application, level) {
+                            var applicationForGrid = {
+                                name: application.name,
+                                applicationType: application.applicationType,
+                                host: application.host,
+                                port: application.port,
+                                url: application.url,
+                                version: application.version,
+                                componentName: application.componentName,
+                                processMemory: application.processMemory,
+                                status: application.status,
+                                //,
+
+                                $$treeLevel: level
+                            };
+
+                            applicationsForGrid.push(applicationForGrid);
                         }
+
+                        return dataForGrid;
+                    }
 
                     console.log('New data for subscription! ' + JSON.stringify(/* $scope.applications */ msg.body.payload.jsonContent));
                 }).withBodyInJson().connect();
